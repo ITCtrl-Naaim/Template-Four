27258dde4c34098ec0a91724aa3af93e05a2ec1a
itcrl-naaim <naaimkraiem@gmail.com>
2024-12-10 21:50:01 +0300

    Settings Page Is Initialized

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Tasks Delete Button
 const deleteBtn = document.querySelectorAll(".latest-tasks .delete");
 const articles = document.querySelectorAll(".latest-tasks .delete");
 
@@ -5,4 +6,17 @@ deleteBtn.forEach((btn) => {
   btn.addEventListener("click", (ele) => {
     ele.currentTarget.closest("article").classList.toggle("done");
   });
+});
+
+// Add Active Class To The NavLink That Is Clicked
+const navLinks = document.querySelectorAll("aside nav a");
+const currentPath = window.location.pathname;
+
+navLinks.forEach((link) => {
+  // Compare the href attribute with the current path
+  if (link.getAttribute("href") === currentPath) {
+    link.classList.add("active");
+  } else {
+    link.classList.remove("active");
+  }
 });
